33802193548593703fb414482738db94fb0a91fa
siarheitsy <modernromaioi@gmail.com>
2024-07-19 19:50:05 +0300

    First fxed interfaces, animal name, bowl

--- src/GeometricFigure.java
+++ src/GeometricFigure.java
@@ -1,22 +1,2 @@
-public interface GeometricFigure {
-
-    default double calculatePerimeter(double sideA) {
-        return sideA * 4;
-    }
-
-    default double calculateArea(double sideA) {
-        return sideA * sideA;
-    }
-
-    default String defaultFillColour() {
-        return "бирюзовый";
-    }
-
-    default String defaultBorderColour() {
-        return "малиновый";
-    }
-
-    void print();
-
-    boolean check();
+package PACKAGE_NAME;public interface GeometricFigure {
 }
