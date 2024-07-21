305c55202e5732fc845d7de57dcf9dc4e13ec4bb
siarheitsy <modernromaioi@gmail.com>
2024-07-21 20:22:28 +0300

    renamed row

--- src/MyArrayDataException.java
+++ src/MyArrayDataException.java
@@ -1,7 +1,7 @@
 public class MyArrayDataException extends Exception {
 
-    public MyArrayDataException(String message, int raw, int column) {
-        super(message + " [" + raw + "][" + column + "]");
+    public MyArrayDataException(String message, int row, int column) {
+        super(message + " [" + row + "][" + column + "]");
     }
 
 }
