eba9a42c8383dab4dc94951c26d558f1b909a956
siarheitsy <modernromaioi@gmail.com>
2024-08-15 22:05:26 +0300

    Added trycatch

--- src/main/java/WebDriverInstance/WebDriverInstance.java
+++ src/main/java/WebDriverInstance/WebDriverInstance.java
@@ -1,6 +1,7 @@
 package WebDriverInstance;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -27,13 +28,16 @@ public class WebDriverInstance {
     private static WebDriver Initialize() {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\ChromeWebDrivers\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().implicitlyWait(2500, TimeUnit.MILLISECONDS);
         driver.get("http://www.mts.by/");
         driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(@class,\"cookie__cancel\")]")));
-        WebElement cookieDisagreeBtn = driver.findElement(By.xpath("//button[contains(@class,\"cookie__cancel\")]"));
-        cookieDisagreeBtn.click();
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(@class,\"cookie__cancel\")]")));
+            WebElement cookieDisagreeBtn = driver.findElement(By.xpath("//button[contains(@class,\"cookie__cancel\")]"));
+            cookieDisagreeBtn.click();
+        } catch (TimeoutException ex) {
+        }
         return driver;
     }
 
