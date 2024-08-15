2d016ed53625bda3c072e90979df08beb21d37f3
siarheitsy <modernromaioi@gmail.com>
2024-08-15 21:42:13 +0300

    Specified chromedriver and window size

--- src/main/java/WebDriverInstance/WebDriverInstance.java
+++ src/main/java/WebDriverInstance/WebDriverInstance.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverInstance {
@@ -22,9 +25,13 @@ public class WebDriverInstance {
     }
 
     private static WebDriver Initialize() {
+        System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\ChromeWebDrivers\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(1500, TimeUnit.MILLISECONDS);
         driver.get("http://www.mts.by/");
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(@class,\"cookie__cancel\")]")));
         WebElement cookieDisagreeBtn = driver.findElement(By.xpath("//button[contains(@class,\"cookie__cancel\")]"));
         cookieDisagreeBtn.click();
         return driver;
