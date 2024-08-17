7efd26bb5a21baace4d8d4b029bae35d787dc761
siarheitsy <modernromaioi@gmail.com>
2024-08-17 14:48:48 +0300

    More steps

--- src/main/java/WebDriverInstance/WebDriverInstance.java
+++ src/main/java/WebDriverInstance/WebDriverInstance.java
@@ -1,5 +1,6 @@
 package WebDriverInstance;
 
+import io.qameta.allure.Allure;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -28,11 +29,13 @@ public class WebDriverInstance {
         driver.manage().timeouts().implicitlyWait(2500, TimeUnit.MILLISECONDS);
         driver.get("http://www.mts.by/");
         driver.manage().window().setSize(new Dimension(1920, 1032));
+        Allure.step("Initialized WebDriver with chromedriver ver.127.0.6533.119, window size set 1920x1032, Chrome ver.127.0.6533.120");
         try {
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(@class,\"cookie__cancel\")]")));
             WebElement cookieDisagreeBtn = driver.findElement(By.xpath("//button[contains(@class,\"cookie__cancel\")]"));
             cookieDisagreeBtn.click();
+            Allure.step("Cookie modal declined");
         } catch (TimeoutException ex) {
         }
         return driver;
